6f8a489702506c6bd811495c29568facf1fd6396
NADA MILONE <nadamilone@gmail.com>
2022-05-31 12:26:01 -0400

    commit tree cdd0b5f322a656392b9eda910324ef38cb4353ce

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) return false;
+  }
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("civic"));
 
-  console.log("");
+  console.log("Vroom!");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("cabbage"));
+
+  console.log("Vroom Vroom");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("kayak"));
 }
 
 module.exports = isPalindrome;
